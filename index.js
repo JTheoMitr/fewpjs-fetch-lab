c1ee3d20941a5ecf626ecb939d885a3b72c3e167
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2021-04-08 21:29:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks(json));
 }
 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
+    const h3 = document.createElement('h3');
     h2.innerHTML = book.name;
     main.appendChild(h2);
+    h3.innerHTML = book.authors;
+    main.appendChild(h3);
   });
 }
 
